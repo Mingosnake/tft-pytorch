7a9f4e2aa052751f14de8a5c8fdd86054310f594
Minseo Kang <kmsid1425@gmail.com>
2021-11-11 17:34:48 +0900

    update model.py

--- core/model.py
+++ core/model.py
@@ -1,6 +1,5 @@
 import torch
 from torch import nn
-import torch.nn.functional as F
 
 
 #Gated Linear Units
@@ -10,10 +9,28 @@ class GateLayer(nn.Module):
 
         self.fc_forward = nn.Linear(input_dim, input_dim)
         self.fc_gate = nn.Linear(input_dim, input_dim)
+        self.sigmoid = nn.Sigmoid()
 
     def forward(self, input):
         #input = [batch size, input dim]
 
-        return F.sigmoid(self.fc_gate(input)) * self.fc_forward(input)
+        return self.sigmoid(self.fc_gate(input)) * self.fc_forward(input)
 
 
+#Gated Residual Network
+class GRN(nn.Module):
+    def __init__(self, input_dim):
+        super().__init__()
+
+        self.fc_input = nn.Linear(input_dim, input_dim)
+        self.fc_context = nn.Linear(input_dim, input_dim, bias=False)
+        self.elu = nn.ELU()
+
+        self.fc_forward = nn.Linear(input_dim, input_dim)
+        self.glu = GateLayer(input_dim)
+
+
+    def forward(self, input):
+        #input = [batch size, input dim]
+
+        pass
